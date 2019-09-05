1fcb9d9c0353b8bcc3b65273d6aa54af0908fb2b
Bernie Reiter <ockham@raz.or.at>
2019-09-06 00:03:09 +0200

    [not verified] [not verified] Add comment

--- extensions/blocks/recurring-payments/edit.jsx
+++ extensions/blocks/recurring-payments/edit.jsx
@@ -55,6 +55,15 @@ class MembershipsButtonEdit extends Component {
 			editedProductRenewInterval: '1 month',
 		};
 		this.timeout = null;
+
+		// FIXME: In Jetpack, we cannot yet support the 'new', Upgrade and Stripe Nudge-based
+		// flow, since the post-checkout redirect skips the Jetpack Onboarding Checklist in Calypso,
+		// where Akismet and VaultPress installation is triggered.
+		// Instead, we continue to show the legacy, in-block prompts to upgrade the site's plan, and to
+		// connect Stripe. This however means that we have to display the block also if the site's on a Free plan.
+		//
+		// Check p7rd6c-23C-p2 for details and progress. If ready, remove the line below, and replace all
+		// checks for `this.isJetpack` with `false`.
 		this.isJetpack = get( window, [ '_currentSiteType' ] ) !== 'simple';
 	}
 
